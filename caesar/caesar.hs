81e592731979632e943f52a8bbaa63bc358aaa12
Peter Hurford <peter@peterhurford.com>
2015-07-23 11:49:45 -0700

    Make Haskell more interactive

--- caesar/caesar.hs
+++ caesar/caesar.hs
@@ -5,4 +5,9 @@ caesar :: [Char] -> Int -> [Char]
 caesar msg shift = chr . (+shift) . ord <$> msg
 
 main :: IO ()
-main = print $ caesar "HELLO" 5
+main = do
+    print "Enter what you would like to caesar shift..."
+    input <- getLine
+    print "Enter the shift amount..."
+    shift <- getLine
+    print $ caesar input $ read shift
