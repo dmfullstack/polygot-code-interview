512722ef7fa1cf60ebf20232fb80340def51c1b1
Peter Hurford <peter@peterhurford.com>
2015-08-19 14:17:35 -0500

    Documentation

--- happy/happy.hs
+++ happy/happy.hs
@@ -1,3 +1,10 @@
+-- A happy number is a number defined by the following process: Starting with any positive integer,
+-- replace the number by the sum of the squares of its digits, and repeat the process until the number
+-- either equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+-- Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1
+-- are unhappy numbers (or sad numbers).  In any language you like, write me a program to determine
+-- whether a number is happy or not.
+
 module Main where
 import Data.List
  
@@ -6,7 +13,7 @@ square x = x * x
 digits = unfoldr mdiv
  where mdiv 0 = Nothing
        mdiv n = Just (m, d)
-        where (d, m) = divMod n 10
+         where (d, m) = divMod n 10
  
 happy :: Integer -> Bool
 happy 1 = True
