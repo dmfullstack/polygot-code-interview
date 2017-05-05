6dcbbeede5046a9e36f35b954dcf26534002c70a
Peter Hurford <peter@peterhurford.com>
2017-05-05 12:24:08 -0500

    one line assignment

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -1,8 +1,7 @@
 def quicksort(xs):
     if xs == []:
         return []
-    x = xs[0]
-    xs = xs[1:]
+    x, xs = xs[0], xs[1:]
     return quicksort(filter(lambda n: n <= x, xs)) + [x] + quicksort(filter(lambda n: n > x, xs))
 
 print(quicksort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8]))
