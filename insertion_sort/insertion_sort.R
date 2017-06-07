12e3872369d27f72372b941d72cb169b44024efb
peterhurford <peter@peterhurford.com>
2017-06-07 16:58:59 -0500

    use while..break

--- insertion_sort/insertion_sort.R
+++ insertion_sort/insertion_sort.R
@@ -1,4 +1,4 @@
-insert_at <- function(items, item, pos = length(items) + 1) {
+insert_at <- function(items, item, pos) {
   if (pos < 1 || pos > length(items) + 1) { stop("Index error.") }
   c(head(items, pos-1), item, tail(items, length(items) - pos + 1))
 }
@@ -6,15 +6,12 @@ insert_at <- function(items, item, pos = length(items) + 1) {
 insertion_sort <- function(items) {
   sorted_items <- items[[1]]
   for (i in seq_along(items)) {
-    found <- FALSE
-    for (j in seq_along(sorted_items)) {
-      if (sorted_items[[j]] > items[[i]]) {
-        sorted_items <- insert_at(sorted_items, items[[i]], j)
-        found <- TRUE
-        break
-      }
+    j <- 1
+    while (sorted_items[[j]] < items[[i]]) {
+      j <- j + 1
+      if (j > length(sorted_items)) { break }
     }
-    if (!found) { sorted_items <- insert_at(sorted_items, items[[i]]) }
+    sorted_items <- insert_at(sorted_items, items[[i]], j)
   }
   sorted_items
 }
